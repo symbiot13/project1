725d8940244fd42fc778aa6410294e8ccda0ff84
Vlad <kolesik2605@gmail.com>
2022-01-16 12:15:20 +0300

    optimization of code

--- project1/js/script.js
+++ project1/js/script.js
@@ -6,11 +6,29 @@ let personalMovieDB = {
     genres : [],
     privat : false, 
 };
-const a = prompt("last viewed film",""),
-      b =  prompt("mark",""),
-      c = prompt("last viewed film",""),
-      d =  prompt("mark","");
-      personalMovieDB.movies[a] =b;
-      personalMovieDB.movies[c] =d;
+
       
+
+      for (let i = 0; i < 2; i++) {
+        const a = prompt("last viewed film", ""),
+             b =  prompt("mark", "");
+
+             if (a != null && b !=null &&  a != "" && b!= "" && a.length<50) {
+                personalMovieDB.movies[a] =b; 
+                console.log("done");
+             } else {
+                 console.log("error");
+                 i--;
+             }     
+      }
+      if (personalMovieDB.count <  10) {
+          console.log("low");
+      } else if (personalMovieDB.count >= 10 &&  personalMovieDB.count <= 30){
+      console.log("nice");
+      } else if ( personalMovieDB.count > 30 ){
+          console.log ("sheesh");
+      }else {
+          console.log("error");
+      }
+     
       console.log(personalMovieDB);
